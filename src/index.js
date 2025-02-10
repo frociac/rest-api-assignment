4eb226b5411de8817fba581d872fab86f7e7da9f
Brad <51890675+frociac@users.noreply.github.com>
2025-02-09 21:48:18 -0500

    write endpoints

--- src/index.js
+++ src/index.js
@@ -13,6 +13,46 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+/** @type {Map<string, { id: string, name: string, email: string }>} */
+const users = new Map();
+
+
+app.post('/users', (req, res) => {
+    const { name, email } = req.body;
+
+    if (!name || !email) return res.status(400).json({ error: "Bad Request" });
+
+    const newUser = { id: crypto.randomUUID(), name, email };
+    users.set(newUser.id, newUser) ;
+
+    res.status(201).json(newUser);
+});
+
+app.get('/users/:id', (req, res) => {
+    let user = users.get(req.params.id);
+
+    if (!user) return res.status(404).json({error: "Not Found"});
+
+    return res.status(200).json(user);
+});
+
+app.put('/users/:id', (req, res) => {
+    const {name, email} = req.body;
+
+    if (!users.has(req.params.id)) return res.status(404).json({error: "Not Found"});
+    if (!name || !email) return res.status(400).json({ error: "Bad Request" });
+
+    users.set(req.params.id, { id: req.params.id, name: name, email: email });
+
+    return res.status(200).json(users.get(req.params.id));
+});
+
+app.delete('/users/:id', (req, res) => {
+    if (!users.has(req.params.id)) return res.status(404).json({error: "Not Found"});
+    users.delete(req.params.id);
+    return res.status(204).send();
+});
+
 // Do not touch the code below this comment
 // **************************************************************
 
